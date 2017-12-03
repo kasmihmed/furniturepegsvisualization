429189a2f113d9956b8f174042cdc2c14ce82513
Sidi Kasmi <sidi.kasmi@trioptima.com>
2017-12-03 16:05:13 +0100

    add server performance measuring middleware

---
+++ furniturepegsvisualization/middlewares.py
@@ -0,0 +1,28 @@
+import os
+import psutil
+import logging
+import datetime
+
+THRESHOLD = 2 * 1024 * 1024
+logger = logging.getLogger(__name__)
+
+
+class MemoryUsageMiddleware(object):
+    def process_request(self, request):
+        request._time_received = datetime.datetime.now()
+        request._mem = psutil.Process(os.getpid()).memory_info()
+        request._cpu_percent = psutil.Process(os.getpid()).cpu_times().system
+
+    def process_response(self, request, response):
+        # MEMORY LOGGING in bytes
+        mem = psutil.Process(os.getpid()).memory_info()
+        mem_diff = mem.rss - request._mem.rss
+        # CPU LOGGING
+        response_cpu_percent = psutil.Process(os.getpid()).cpu_times().system
+        cpu_diff = response_cpu_percent - request._cpu_percent
+        # 'cpu_percent', 'cpu_times'
+        # SERVER REQUEST RESPONSE TIME
+        response_time = (datetime.datetime.now() - request._time_received).total_seconds()
+        # log the request/response data
+        logger.error(u'{};{};{};{}'.format(request.path, mem_diff, response_cpu_percent, response_time))
+        return response
